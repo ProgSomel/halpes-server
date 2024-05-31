0b6bebc2f4c2d62da232d624c920c4bdeafe6b4a
ProgSomel <somelahmed88@gmail.com>
2024-05-31 11:39:12 +0600

    Get Volunteer Post Using Id Done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -39,6 +39,14 @@ async function run() {
 
     app.get('/volunteer', async(req, res) => {
       const result = await volunteerPostsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get("/volunteer/:id", async(req, res) => {
+      const id = req.params.id;
+     
+      const query = {_id: new ObjectId(id)};
+      const result = await volunteerPostsCollection.findOne(query);
       res.send(result);
     })
 
