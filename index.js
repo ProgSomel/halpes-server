2628f0ca11b3704258f1f9517a3ed9ac4bf9e72c
ProgSomel <somelahmed88@gmail.com>
2024-06-01 17:48:44 +0600

    Manage Post ----> Delete Functionality Done

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ async function run() {
     })
 
 
+    app.delete('/volunteer/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await volunteerPostsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     //! Be A Volunteer 
     app.post('/beAvolunteer', async (req, res) => {
       const beAvolunteerData = req.body;
